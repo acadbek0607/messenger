e36b193497d7b6adafe2eca79c51059e65c1cfb9
acadbekk <vecious007@gmail.com>
2025-02-15 14:25:02 +0500

    2. MVI

--- presentation/src/main/java/org/acad/presentation/screens/phone/PhoneFragment.kt
+++ presentation/src/main/java/org/acad/presentation/screens/phone/PhoneFragment.kt
@@ -1,9 +1,29 @@
 package org.acad.presentation.screens.phone
 
-import androidx.fragment.app.Fragment
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.View
+import org.acad.domain.model.User
+import org.acad.presentation.base.BaseFragment
+import org.acad.presentation.databinding.FragmentPhoneBinding
+import org.acad.presentation.screens.phone.PhoneVM.Effect
 
 /**
  * Created by Acad Bek on 2/15/2025
  */
-class PhoneFragment : Fragment() {
+class PhoneFragment : BaseFragment<FragmentPhoneBinding>(FragmentPhoneBinding::inflate) {
+
+    private lateinit var vm: PhoneVM
+
+    @SuppressLint("CheckResult")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        vm.state.observe(::renderUser) { it.user!! }
+        vm.effects.doOnNext(::handleEffect)
+
+    }
+
+    private fun renderUser(user: User?) {}
+    private fun handleEffect(effect: Effect) {}
 }
--- presentation/src/main/java/org/acad/presentation/screens/phone/PhoneVM.kt
+++ presentation/src/main/java/org/acad/presentation/screens/phone/PhoneVM.kt
@@ -1,12 +1,27 @@
 package org.acad.presentation.screens.phone
 
 import androidx.lifecycle.ViewModel
+import org.acad.domain.model.User
 import org.acad.domain.usecase.auth.SendSmsCodeUseCase
+import org.acad.presentation.base.BaseVM
+import org.acad.presentation.screens.phone.PhoneVM.*
 
 /**
  * Created by Acad Bek on 2/15/2025
  */
 class PhoneVM constructor(
     private val sendSmsCodeUseCase: SendSmsCodeUseCase
-) : ViewModel() {
+) : BaseVM<State, Event, Effect>() {
+
+    data class State(
+        val user: User? = null
+    )
+
+    class Event
+
+    class Effect
+
+    override fun getDefaultState() = State()
+
+    override fun processEvent(event: Event) {}
 }
